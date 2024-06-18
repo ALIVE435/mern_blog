a207d1c5ba4e0a3a4aa6ecf12f21c3f7b00d4578
ALIVE435 <aniketksingh01@gmail.com>
2024-06-18 19:30:43 +0530

    complete sidebar of dashboard

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -5,7 +5,7 @@ const ThemeProvider = ({children})=>{
     const { theme } = useSelector(state=>state.theme)
     return(
         <div className={theme?"light":"dark"}>
-            <div className="bg-slate-100 text-gray-700 dark:text-gray-200 dark:bg-[rgb(18,26,59)]">
+            <div className="bg-slate-100 text-gray-700 min-h-screen dark:text-gray-200 dark:bg-[rgb(18,26,59)]">
                 {children}
             </div>
         </div>
