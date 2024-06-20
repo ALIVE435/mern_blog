697d34295de5f165ae73ce574287462c5c1cfa88
ALIVE435 <aniketksingh01@gmail.com>
2024-06-20 17:31:32 +0530

    change profile image functionality using firebase

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,24 +1,78 @@
 import { useSelector } from "react-redux"
-import { Button, TextInput } from "flowbite-react"
-
-
-
+import { Alert, Button, TextInput } from "flowbite-react"
+import { useState, useRef, useEffect } from "react";
+import { getDownloadURL, getStorage, ref, uploadBytesResumable, } from 'firebase/storage'
+import { app } from "../firebase";
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 export const DashProfile = () => {
     const { currentUser } = useSelector(state => state.user);
+    const [imageFile, setImageFile] = useState(null);
+    const [imageFileUrl, setImageFileUrl] = useState(null);
+    const fileRef = useRef(null);
+    const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null)
+    const [imageFileUploadError, setImageFileUploadError] = useState(null)
 
+    const handleFileChange = (e) => {
+        const image = e.target.files[0];
+        if (image) {
+            setImageFile(image)
+            setImageFileUrl(URL.createObjectURL(image))
+        }
+    }
+    const uploadImage = async () => {
+        const imageStorage = getStorage(app);
+        const fileName = new Date().getTime() + imageFile.name;
+        // console.log(fileName)
+        const storageRef = ref(imageStorage, fileName);
+        // console.log(storageRef)
+        const uploadTask = uploadBytesResumable(storageRef, imageFile);
+        // console.log(uploadTask)
+        uploadTask.on('state_changed', 
+        (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            setImageFileUploadProgress(progress.toFixed(0));
+        },
+        (error) => {
+            setImageFileUploadError("couldn't upload file (file must be less than 2mb)")
+        },
+        () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    console.log(downloadURL)
+                    setImageFileUrl(downloadURL);
+                })
+            }
+        )
+    };
+
+    useEffect(() => {
+        if (imageFile) uploadImage()
+    }, [imageFile])
 
     return (
         <div className="max-w-lg mx-auto w-full">
             <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
             <form className="flex flex-col gap-4">
-                <div className="w-32 h-32 self-center cursor-pointer shadow-md rounded-full overflow-hidden">
-                    <img src={currentUser.photoUrl} alt="user" className="rounded-full h-full border-8 border-[lightGray]" />
+                <input type="file" accept="image/*" onChange={handleFileChange} ref={fileRef} hidden />
+                <div className="w-32 h-32 self-center cursor-pointer shadow-md rounded-full overflow-hidden relative">
+                    {imageFileUploadProgress && <CircularProgressbar value={imageFileUploadProgress || 0} 
+                        text={`${imageFileUploadProgress}%`} 
+                        strokeWidth={5} 
+                        styles={{
+                            root:{width:"100%",height:"100%",position:"absolute",top:"0",left:"0"},
+                            path:{stroke:`rgba(62,152,199), ${imageFileUploadProgress}/100`}
+                        }}/>
+                    }
+                    <img src={imageFileUrl || currentUser.photoUrl} alt="user" onClick={() => fileRef.current.click()} className="rounded-full w-full object-cover h-full border-8 border-[lightGray]" />
                 </div>
+                {imageFileUploadError && <Alert color={'failure'}>
+                    {imageFileUploadError}
+                </Alert>}
                 <TextInput type="text" id='username' placeholder='username' defaultValue={currentUser.username} />
                 <TextInput type="email" id='email' placeholder={"email"} defaultValue={currentUser.email} />
                 <TextInput type="password" placeholder="password" id="password" />
-                <Button type="submit" gradientDuoTone={"purpleToBlue"}  outline>
+                <Button type="submit" gradientDuoTone={"purpleToBlue"} outline>
                     Update
                 </Button>
                 <div className="text-red-500 flex justify-between">
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useLocation,Link } from "react-router-dom"
+import { useLocation, Link } from "react-router-dom"
 import { Sidebar } from "flowbite-react";
 import { HiArrowSmRight, HiUser } from "react-icons/hi"
 
@@ -12,18 +12,17 @@ export const DashSidebar = () => {
         const urlParams = new URLSearchParams(location.search);
         const tabFromUrl = urlParams.get("tab");
         if (tabFromUrl) setTab(tabFromUrl);
-
     }, [location.search]);
     return (
         <Sidebar className="w-full md:w-56">
             <Sidebar.Items>
                 <Sidebar.ItemGroup>
-                    <Link to="/dashboard?tab=profile" >
-                        <Sidebar.Item as={"div"} active={tab == 'profile'} icon={HiUser} label={"User"} labelColor='dark'>
+                    <Link to="/dashboard?tab=profile" >  { /*<a><a></a></a> not possible hence internal link is marked as div*/}
+                        <Sidebar.Item as={"div"} active={tab === 'profile'} icon={HiUser} label={"User"} labelColor='dark'>
                             Profile
                         </Sidebar.Item>
                     </Link>
-                    <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>
+                    <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
                         SignOut
                     </Sidebar.Item>
                 </Sidebar.ItemGroup>
