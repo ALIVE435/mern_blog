cb6756eb4f6f15ee25f608839b38dbf983fb418b
ALIVE435 <aniketksingh01@gmail.com>
2024-07-01 19:18:09 +0530

    created getposts api route

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { useLocation, Link } from "react-router-dom"
 import { Sidebar } from "flowbite-react";
-import { HiArrowSmRight, HiUser } from "react-icons/hi"
-import { useDispatch } from "react-redux";
+import { HiArrowSmRight, HiDocumentText, HiUser } from "react-icons/hi"
+import { useDispatch, useSelector } from "react-redux";
 import { signoutSuccess } from "../redux/user/userSlice";
 import axios from "axios";
 
@@ -10,6 +10,7 @@ export const DashSidebar = () => {
     const location = useLocation();
     const [tab, setTab] = useState("");
     const dispatch = useDispatch();
+    const { currentUser } = useSelector((state) => state.user)
 
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search);
@@ -30,13 +31,19 @@ export const DashSidebar = () => {
     return (
         <Sidebar className="w-full md:w-56">
             <Sidebar.Items>
-                <Sidebar.ItemGroup>
-                    <Link to="/dashboard?tab=profile" >  { /*<a><a></a></a> not possible hence internal link is marked as div*/}
-                        <Sidebar.Item as={"div"} active={tab === 'profile'} icon={HiUser} label={"User"} labelColor='dark'>
+                <Sidebar.ItemGroup className="flex flex-col gap-y-0.5">
+                    <Link to="/dashboard?tab=profile">  { /*<a><a></a></a> not possible hence internal link is marked as div*/}
+                        <Sidebar.Item as={"div"} active={tab === 'profile'} icon={HiUser} label={currentUser.isAdmin?'Admin':"User"} labelColor='dark'>
                             Profile
                         </Sidebar.Item>
                     </Link>
-                    <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer" onClick={handleSignOut}>
+                    {currentUser.isAdmin && 
+                    <Link to="/dashboard?tab=posts" >  { /*<a><a></a></a> not possible hence internal link is marked as div*/}
+                        <Sidebar.Item as={"div"} active={tab === 'posts'} icon={HiDocumentText}>
+                            Posts
+                        </Sidebar.Item>
+                    </Link>}
+                    <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer my-0" onClick={handleSignOut}>
                         SignOut
                     </Sidebar.Item>
                 </Sidebar.ItemGroup>
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,28 +1,32 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom"
-import {DashSidebar} from "../components/DashSidebar"
-import {DashProfile} from "../components/DashProfile"
+import { DashSidebar } from "../components/DashSidebar"
+import { DashProfile } from "../components/DashProfile"
+import DashPost from "../components/DashPost";
 
 
 export default function Dashboard() {
   const location = useLocation();
-  const [tab,setTab] = useState("");
+  const [tab, setTab] = useState("");
 
-  useEffect(()=>{
+  useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get("tab");
-    if(tabFromUrl) setTab(tabFromUrl);
+    if (tabFromUrl) setTab(tabFromUrl);
 
-  },[location.search]);
+  }, [location.search]);
 
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
       <div className="md:w-56">
-        <DashSidebar/>
+        <DashSidebar />
       </div>
-      <div className="w-full">
-        <DashProfile/>
-      </div>
+      {(tab === 'posts') ?
+        <div className="w-full">
+          <DashPost />
+        </div> : <div className="w-full">
+          <DashProfile />
+        </div>}
     </div>
   )
 }
